3066f12f19aeb52b52ceb3704473006b2e53e37b
SikoraKam <sikorakam3@gmail.com>
2021-04-24 13:27:04 +0200

    Reformat with prettier

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,9 +4,9 @@ import { Center } from "@chakra-ui/react";
 import { Logo } from "@components";
 
 export const Header: React.FC = () => {
-    return (
-        <Center bg="header.100">
-            <Logo />
-        </Center>
-    );
+  return (
+    <Center bg="header.100">
+      <Logo />
+    </Center>
+  );
 };
