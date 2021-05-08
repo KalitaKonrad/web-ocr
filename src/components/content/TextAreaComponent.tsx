638b05b040b12d31ae24a91843249bc5beb45934
SikoraKam <sikorakam3@gmail.com>
2021-05-08 16:33:05 +0200

    feat: addjust text area

--- src/components/content/TextAreaComponent.tsx
+++ src/components/content/TextAreaComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Textarea } from "@chakra-ui/react";
 
 interface TextAreaProps {
@@ -6,16 +6,28 @@ interface TextAreaProps {
 }
 
 const TextAreaComponent: React.FC<TextAreaProps> = ({ detectedText }) => {
+  const [textAreaValue, setTextAreaValue] = useState(detectedText);
+
+  useEffect(() => {
+    setTextAreaValue(detectedText);
+  }, [detectedText]);
+
+  const handleChange = (event) => {
+    setTextAreaValue(event.target.value);
+  };
+
   return (
-    <Box my={5}>
+    <Box mt={5} flex={1}>
       <Textarea
+        height="100%"
         borderWidth={0}
         placeholder="Dodaj plik aby rozpocząć rozpoznawanie tekstu"
         resize="none"
         size="lg"
         variant="outline"
         isDisabled={detectedText === ""}
-        value={detectedText}
+        value={textAreaValue}
+        onChange={handleChange}
       />
     </Box>
   );
